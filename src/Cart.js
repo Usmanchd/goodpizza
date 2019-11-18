5b241b5a35996895a603c846fbbb93c00979b837
Usman <usman.aslam0701@gmail.com>
2019-11-18 21:55:58 +0500

    m

---
+++ src/Cart.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import './Cart.css';
+
+function Buttons(props) {
+  if (props.item.count === 1) {
+    return (
+      <div className="Cartbuttons">
+        <button onClick={() => props.removeCart(props.item)}>
+          Remove Item {props.item.count}
+        </button>
+        <button onClick={() => props.addSingle(props.item)}>+</button>
+      </div>
+    );
+  } else {
+    return (
+      <div className="Cartbuttons">
+        <button onClick={() => props.removeSingle(props.item)}>-</button>
+        <button onClick={() => props.removeCart(props.item)}>
+          Remove Item {props.item.count}
+        </button>
+        <button onClick={() => props.addSingle(props.item)}>+</button>
+      </div>
+    );
+  }
+}
+
+function Cart(props) {
+  return (
+    <div className="Cart">
+      {/* <h1>Cart</h1> */}
+      {props.item.map((c, index) => {
+        return (
+          <div key={index} className="cartitems">
+            <p>
+              <b>Name :</b>
+              {c.name}
+            </p>
+            {/* <p>
+                    <b>Description :</b>
+                    {c.description}
+                  </p> */}
+            <p>
+              <b>Price :</b>
+              {c.showprice}
+            </p>
+            {/* <p>
+                    <b>Count :</b>
+                    {c.count}
+                  </p> */}
+            <Buttons
+              item={c}
+              removeSingle={props.removeSingle}
+              addSingle={props.addSingle}
+              removeCart={props.removeCart}
+            />
+            {/* <button
+                className="cross"
+                onClick={() => props.removeCart(c)}
+              >
+                X
+              </button> */}
+          </div>
+        );
+      })}
+      <h3 className="totalp">Totalprice : {props.totalPrice}</h3>
+      <button className="btncheckout" onClick={() => props.checkOut()}>
+        Check Out
+      </button>
+    </div>
+  );
+}
+
+export default Cart;
---
+++ src/Items.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import './Items.css';
+import logo1 from './img/1.jpg';
+import logo2 from './img/2.jpg';
+import logo3 from './img/3.jpg';
+import logo4 from './img/4.jpg';
+import logo5 from './img/5.jpg';
+import logo6 from './img/6.jpg';
+function Items(props) {
+  const products = [
+    {
+      name: 'Anday wala burger',
+      image: logo1,
+      Price: 240,
+      description:
+        'Anday wala burger is best in Pakistan mje andaya wala burger.'
+    },
+    {
+      name: 'Shwarma',
+      image: logo2,
+      Price: 150,
+      description:
+        'Shwarma with extra chiken and socies is best in Pakistan you can taste the best shwarma here.'
+    },
+    {
+      name: 'Lazania Pizza',
+      image: logo3,
+      Price: 540,
+      description:
+        'Lizania has spicy and juicy tast and it is best in Pakistan Chiken Lazania'
+    },
+    {
+      name: 'Zinger burger',
+      image: logo4,
+      Price: 160,
+      description:
+        'Zinger burger is hot and crispy burger and this is the best in Pakistan Burger Zingor'
+    },
+    {
+      name: 'Zinger burger',
+      image: logo5,
+      Price: 160,
+      description:
+        'Zinger burger is hot and crispy burger and this is the best in Pakistan Burger Zingor'
+    },
+    {
+      name: 'Zinger burger',
+      image: logo6,
+      Price: 160,
+      description:
+        'Zinger burger is hot and crispy burger and this is the best in Pakistan Burger Zingor'
+    }
+  ];
+
+  return (
+    <div className="itemsPage">
+      {products.map((product, index) => {
+        return (
+          <div key={index} className="grid">
+            <img src={product.image} alt="itemImage" />
+            <div className="d">
+              <p>
+                {/* <b>Name :</b> */}
+                {product.name}
+              </p>
+              {/* <p>
+                <b>Description :</b>
+                {product.description}
+              </p> */}
+              <p>
+                {/* <b>Price :</b> */}
+                {product.Price} Rs
+              </p>
+              <button
+                className="btn smoothscroll pb_outline-light rounded-0 btn-xl pb_font-13 pb_letter-spacing-2 p-3"
+                onClick={() => props.addCart(product)}
+              >
+                Add to Cart
+              </button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Items;
