6d845f2ff119382ab6dfc5cc816d78146e8100cc
Usman <usman.aslam0701@gmail.com>
2019-11-18 22:11:11 +0500

    Updates

---
+++ precache-manifest.10183a5deb7851864a7b7c3ad322125b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "803dd9f860512af41a0cfb07136d58e0",
+    "url": "/goodpizza/index.html"
+  },
+  {
+    "revision": "f135244da624d39c6ed2",
+    "url": "/goodpizza/static/css/main.4445e301.chunk.css"
+  },
+  {
+    "revision": "d6818c0408319f4923e2",
+    "url": "/goodpizza/static/js/2.35fe1d6e.chunk.js"
+  },
+  {
+    "revision": "f135244da624d39c6ed2",
+    "url": "/goodpizza/static/js/main.fedaead6.chunk.js"
+  },
+  {
+    "revision": "6cb80ac36b14426364ff",
+    "url": "/goodpizza/static/js/runtime-main.f8c329b8.js"
+  },
+  {
+    "revision": "c81cea7cc8d78a9e864c08f896d261db",
+    "url": "/goodpizza/static/media/1.c81cea7c.jpg"
+  },
+  {
+    "revision": "142f63c668f258ba5a4f4347f9fef5cc",
+    "url": "/goodpizza/static/media/2.142f63c6.jpg"
+  },
+  {
+    "revision": "485ec987c3c7aaa8d9be052540a3b022",
+    "url": "/goodpizza/static/media/3.485ec987.jpg"
+  },
+  {
+    "revision": "0998b5b63b6bc42f70b291d1803aa5c7",
+    "url": "/goodpizza/static/media/4.0998b5b6.jpg"
+  },
+  {
+    "revision": "019e3c43e6d6a3ea5a4368a426388d85",
+    "url": "/goodpizza/static/media/5.019e3c43.jpg"
+  },
+  {
+    "revision": "0b94137bbed456e3e22b2b03ea99e58e",
+    "url": "/goodpizza/static/media/6.0b94137b.jpg"
+  }
+]);
